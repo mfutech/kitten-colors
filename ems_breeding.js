5776f6e2217a69e661c2eb065f0be22a371c5e47
mfutech <mfutech@users.noreply.github.com>
2019-11-25 22:18:55 +0100

    adding licence

--- ems_breeding.js
+++ ems_breeding.js
@@ -1,10 +1,24 @@
+/** Kitten Genotype Calculator
+    Copyright (C) 2019 Marc Furrer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
 
 /*
     ems_breeding.js
+    function o compute breeding combinations
     
-    Author: Marc Furrer, 2019
-
-    compose possible kitten colors based parent EMS color codes
 */
 
 "use strict"
--- ems_genotype.js
+++ ems_genotype.js
@@ -1,3 +1,19 @@
+/** Kitten Genotype Calculator
+    Copyright (C) 2019 Marc Furrer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
 /*
     ems_genotype.js
     
@@ -119,5 +135,5 @@ function ems_genotype(ems_code, sex = 'male') {
 };
 
 function translate_genotype_color (genotype){
-    return("beau");
+    
 }
