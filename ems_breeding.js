a2f8856644b0d24eb5d4d54f17d3c93b0c96feb6
mfutech <mfutech@users.noreply.github.com>
2019-11-27 23:10:19 +0100

    add genotype to text translation (in french)

--- ems_breeding.js
+++ ems_breeding.js
@@ -29,6 +29,97 @@ function uniq (arr) {
     );
 }
 
+class CatColor{
+
+    constructor(base, dilution, fullcolor){
+        this.base = base;
+        this.dilution = dilution;
+        this.fullcolor = fullcolor;
+        this.genotype = genotype2Str([this.base, this.dilution, this.fullcolor]);
+        this.diluted = (dilution == 'dd');
+        this.cinamon = (fullcolor = 'blbl');
+        this.sex = (base.slice(-1) == "y" ? "male" : "femelle");
+        let c = [ this.fullcolor, this.dilution, this.base].join(" ");
+        let res;
+        //console.log(c);
+        if (c.match(/B. D. o[oy]/))          res = ["black",    "n"];
+        else if (c.match(/B. dd o[oy]/))     res = ["blue",     "a"];
+        else if (c.match(/blbl D. o[oy]/))   res = ["cinnamon", "o"];
+        else if (c.match(/blbl dd o[oy]/))   res = ["fawn",     "p"];
+        else if (c.match(/[bl/]+ dd o[oy]/)) res = ["lilac",    "c"];
+        else if (c.match(/B. D. O[Oy]/))     res = ["red",      "d"];
+        else if (c.match(/b. D. O[Oy]/))     res = ["red",      "d"];
+        else if (c.match(/blbl D. O[Oy]/))   res = ["red",      "d"];
+        else if (c.match(/B. dd O[Oy]/))     res = ["cream",    "e"];
+        else if (c.match(/b. dd O[Oy]/))     res = ["cream",    "e"];
+        else if (c.match(/blbl dd O[Oy]/))   res = ["cream",    "e"];
+        else if (c.match(/-- dd o[oy]/))     res = ["blue(fawn?)", "a/p?"];
+        else if (c.match(/B. D. Oo/))        res = ["black tortie", "f"];
+        else if (c.match(/B. dd Oo/))        res = ["chocolatetortie", "h"];
+        else if (c.match(/bbl? D. Oo/))      res = ["lilactortie", "j"];
+        else if (c.match(/blbl dd Oo/))      res = ["cinnamontortie", "q"];
+        else if (c.match(/-- D. o[oy]/))     res = ["black(cinnamon?)", "n/o?"];
+        else if (c.match(/B. -- o[oy]/))     res = ["black(blue?)", "n/a?"];
+        else if (c.match(/-- -- o[oy]/))     res = ["black ???", "n??"];
+        else res = ["???", "?"];
+
+        //console.log(res);
+        this.color = res[0];
+        this.color_ems = res[1];
+        this.text = this.sex + " " + this.color + " (" + this.color_ems + ")";
+    };
+};
+
+class CatSilver {
+    constructor(silver){
+        this.silver = silver;
+        this.genotype = silver
+        switch (silver) {
+            case 'ii': 
+                this.text = "non-silver"; break;
+            case "i-":
+                this.text = "non-silver?"; break;
+            default:
+                this.text = "silver";
+        };
+    };
+};
+
+class CatAggouti {
+    constructor (aggouti) {
+        this.aggouti = aggouti;
+        this.genotype = aggouti; 
+        switch (aggouti) {
+            case "aa":
+               this.text = "solid"; break;
+            case "a-":
+                this.text = "solid?"; break;
+            case "Aa":
+                this.text = "aggouti (porteur solid)"; break;
+            default:
+                this.text = "aggouti";
+        };
+    }
+};
+
+class CatPoint {
+    constructor (point) {
+        this.point = point;
+        this.genotype = point;
+        switch(point) {
+            case "cscs":
+                this.text = "point"; break;
+            case "Cscs":
+                this.text = "classic (porteur point)"; break;
+            case "cs-":
+                this.text = "point ?"; break;
+            default:
+                this.text = "classic";
+        };
+    };
+
+};
+
 function compose_gen(gen1, gen2) {
     let res = [ gen1, gen2 ].sort().join("");
     if (res[0] == "-") { // if first caracter is "-" then if must be put at the end
@@ -48,7 +139,7 @@ function combine_genes ( gen_arr1, gen_arr2 ) {
 };
 
 function genotype2Str ( genotype_arr ) {
-    console.log(genotype_arr);
+    // console.log(genotype_arr);
     return uniq(genotype_arr).sort((a, b) => 
         a.localeCompare(b, undefined, {sensitivity: 'base'})
         ).join(" ");
@@ -77,16 +168,16 @@ function ems_breeding(father_ems, mother_ems) {
     full_result.basic_color.forEach(bc =>
         full_result.diluted_color.forEach(dc =>
             full_result.fullcolor_color.forEach(fc=>
-                colors.push( genotype2Str([bc, dc, fc])) 
+                colors.push( new CatColor(bc, dc, fc)) 
             )
         )
     );
 
     let result = {
         colors:     colors,
-        silver:     full_result.silver_color,
-        aggouti:    full_result.modifier_aggouti,
-        siamese:    full_result.modifier_siamese,
+        silver:     full_result.silver_color.map(c => new CatSilver(c)),
+        aggouti:    full_result.modifier_aggouti.map(c => new CatAggouti(c)),
+        siamese:    full_result.modifier_siamese.map(c => new CatPoint(c)),
     };
 
     return result;
